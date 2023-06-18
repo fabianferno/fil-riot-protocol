34e18056a3bc9fdc2b3c20b7264ce745bbb23b00
Fabian Ferno <fabianferno@gmail.com>
2023-06-18 21:02:55 +0530

    Add spheron storage deals

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
 import * as dotenv from "dotenv";
+import { dataDump } from "./helpers/dataDump";
+const cron = require("node-cron");
 
 dotenv.config();
 
 import server from "./api";
+
+cron.schedule("0 0 * * *", function () {
+  // Run every day at midnight
+  dataDump();
+});
 
 server.listen(parseInt(process.env.PORT || "5000"), "0.0.0.0", () => {
   // Initialize the LIT SDK
